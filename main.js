64cc483706bc16a48dab1b3c8a924d55f57679ff
Yuri Brame <yuribrame@gmail.com>
2022-11-15 11:31:42 -0300

    Add final details

--- main.js
+++ main.js
@@ -8,9 +8,11 @@ function createGame(player1, hour, player2) {
   `
 }
 
+let delay = -0.4;
 function createrCard(date, day, games) {
+  delay = delay + 0.4;
   return `
-   <div class="card">
+   <div class="card" style="animation-delay: ${delay}s">
             <h2>${date}<span>${day}</span></h2>
             <ul>
                ${games}
@@ -18,34 +20,28 @@ function createrCard(date, day, games) {
         </div>`
 }
 
-document.querySelector("#app").innerHTML = `
-  <header>
-        <img src="./assets/logo.svg" alt="Logo da NLW">
-    </header>
-    <main id="cards">
-        ${createrCard(
-          "24/11",
-          "quinta",
-          createGame("switzerland", "07:00", "cameroon") +
-            createGame("uruguay", "10:00", "southKorea") +
-            createGame("portugal", "16:00", "ghana") +
-            createGame("brazil", "16:00", "serbia")
-        )}
-        ${createrCard(
-          "28/11",
-          "segunda",
-          createGame("cameroon", "07:00", "serbia") +
-            createGame("southKorea", "10:00", "ghana") +
-            createGame("brazil", "13:00", "switzerland") +
-            createGame("portugal", "16:00", "uruguay")
-        )}
-        ${createrCard(
-          "02/12",
-          "sexta",
-          createGame("southKorea", "12:00", "portugal") +
-            createGame("ghana", "12:00", "uruguay") +
-            createGame("serbia", "16:00", "switzerland") +
-            createGame("brazil", "16:00", "cameroon")
-        )}
-    </main>
-`
+document.querySelector("#cards").innerHTML = 
+  createrCard(
+    "24/11",
+    "quinta",
+    createGame("switzerland", "07:00", "cameroon") +
+      createGame("uruguay", "10:00", "southKorea") +
+      createGame("portugal", "16:00", "ghana") +
+      createGame("brazil", "16:00", "serbia")
+  ) +
+  createrCard(
+    "28/11",
+    "segunda",
+    createGame("cameroon", "07:00", "serbia") +
+      createGame("southKorea", "10:00", "ghana") +
+      createGame("brazil", "13:00", "switzerland") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createrCard(
+    "02/12",
+    "sexta",
+    createGame("southKorea", "12:00", "portugal") +
+      createGame("ghana", "12:00", "uruguay") +
+      createGame("serbia", "16:00", "switzerland") +
+      createGame("brazil", "16:00", "cameroon")
+  )
